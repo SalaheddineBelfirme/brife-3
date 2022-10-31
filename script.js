ef245a05b675de97d5eb1f1ff34f55318cbf6103
salaheddien <belfirmesalaheddine@gmail.com>
2022-10-31 23:18:05 +0100

    livrable 3

---
+++ script.js
@@ -0,0 +1,58 @@
+
+
+
+ function myFunction() {
+    var element = document.getElementById("nlinks");
+    var CSS = element.style.visibility;
+    if(CSS != "hidden"){
+      element.style.visibility = "hidden";
+    }else if(CSS == "hidden"){
+      element.style.visibility = "visible";
+    }
+    window.addEventListener('resize',function(){
+     
+          if( this.window.innerWidth >700){
+              document.getElementById("nlinks").style.visibility="visible";
+          }
+       });
+ }
+// for filterage 
+ function ff(){
+  const fltr = document.querySelector('.cards-filtr-select');
+  function toutMune(){//show all prodact
+    document.getElementById("Sushi").style.display="block";
+    document.getElementById("Salade").style.display="block";
+    document.getElementById("Plats").style.display="block";
+  }
+   if(fltr.value =="Salade"){
+    toutMune();
+      document.getElementById("Sushi").style.display="none";
+      document.getElementById("Plats").style.display="none";
+   }
+  else if(fltr.value =="Plats"){
+    toutMune();
+      document.getElementById("Sushi").style.display="none";
+      document.getElementById("Salade").style.display="none";
+    }
+   else if(fltr.value =="Sushi"){
+      toutMune();
+      alert("lllllll");
+      document.getElementById("Plats").style.display="none";
+      document.getElementById("Salade").style.display="none";
+    }
+    else if(fltr.value =="Toutes les categories"){
+    toutMune();
+    }
+
+ }
+ 
+function paine(id){
+  var plase =document.getElementById(id);
+  var card= plase.parentElement.parentElement;
+   var img =card.getElementsByTagName("img")[0].src;
+   var name =card.getElementsByTagName("span")[0].textContent;
+   var price =card.getElementsByTagName("span")[1].textContent;
+   console.log(img);
+   document.querySelector(".product").innerHTML+='<div class="cardCA">  <span>'+name+'</span> <div class="img-card">   <img src="'+img+'" alt=""> </div> <div class="prix">  <span>'+price+'</span>  </div> </div>';
+}
+
